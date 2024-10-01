5d7ef2c77c8c758ce40c10e9aed61eb4b4729f1e
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-01 16:32:11 +0530

    Update 513_Janmansh_and_Assignments.cpp

--- 513_Janmansh_and_Assignments.cpp
+++ 513_Janmansh_and_Assignments.cpp
@@ -1,29 +1,29 @@
 /*
-    Janmansh and Assignments  
+    Janmansh and Assignments
 Janmansh has to submit 3 assignments for Chingari before 10 pm and he starts to do the assignments at $X$ pm.
-Each assignment takes him 1 hour to complete. Can you tell whether he'll be able to complete all assignments on time or not?  
+Each assignment takes him 1 hour to complete. Can you tell whether he'll be able to complete all assignments on time or not?
 
-Input Format  
-- The first line will contain $T$ - the number of test cases. Then the test cases follow.  
-- The first and only line of each test case contains one integer $X$ - the time when Janmansh starts doing the assignments.  
+Input Format
+- The first line will contain $T$ - the number of test cases. Then the test cases follow.
+- The first and only line of each test case contains one integer $X$ - the time when Janmansh starts doing the assignments.
 
-Output Format  
-For each test case, output Yes if he can complete the assignments on time. Otherwise, output No.  
+Output Format
+For each test case, output Yes if he can complete the assignments on time. Otherwise, output No.
 You may print each character of Yes and No in uppercase or lowercase (for example, yes, yEs, yEs, YES will be considered identical).
 
 */
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    int t,x;
+    int t, x;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         cin >> x;
-        if(x <= 7)
+        if (x <= 7)
         {
             cout << "Yes" << endl;
         }
