a9032b40686ba8001edd457b97c2d7feebe1b717
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-19 17:50:50 +0530

    Update 546_MInimum_Pizza.cpp

--- 546_MInimum_Pizza.cpp
+++ 546_MInimum_Pizza.cpp
@@ -21,3 +21,14 @@ int main() {
         }
     }
 }
+/*
+Minimum Pizzas
+Each pizza consists of 4 slices. There are N friends and each friend needs exactly X slices.
+Find the minimum number of pizzas they should order to satisfy their appetite.
+Input Format
+• The first line of input will contain a single integer 1, denoting the number of test cases.
+• Each test case consists of two integers N and X, the number of friends and the number of slices each friend
+wants respectively.
+Output Format
+For each test case, output the minimum number of pizzas required.
+*/
