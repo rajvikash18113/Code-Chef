dc979c081909c1738140fbffcb8cc40c895571ad
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-19 17:50:42 +0530

    Update 541_Sale_Season.cpp

--- 541_Sale_Season.cpp
+++ 541_Sale_Season.cpp
@@ -28,3 +28,17 @@ int main() {
 	}
 
 }
+/*
+Sale Season
+It's the sale season again and Chef bought items worth a total of X rupees. The sale season offer is as follows:
+• if X ≤ 100, no discount.
+• if 100 < X ≤ 1000, discount is 25 rupees.
+• if 1000 < X ≤ 5000, discount is 100 rupees.
+• if X > 5000, discount is 500 rupees.
+Find the final amount Chef needs to pay for his shopping.
+Input Format
+• The first line of input will contain a single integer T, denoting the number of test cases.
+• Each test case consists of single line of input containing an integer X.
+Output Format
+For each test case, output on a new line the final amount Chef needs to pay for his shopping.
+*/
