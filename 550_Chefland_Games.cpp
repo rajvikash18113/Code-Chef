56aebc04900339fd0e19a392f188820f7ddf1d0d
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-21 19:41:22 +0530

    Update 550_Chefland_Games.cpp

--- 550_Chefland_Games.cpp
+++ 550_Chefland_Games.cpp
@@ -22,3 +22,19 @@ int main() {
     }
 
 }
+/*
+Chefland Games
+In Chefland, a tennis game involves 4 referees.
+Each referee has to point out whether he considers the ball to be inside limits or outside limits. The ball is
+considered to be IN if and only if all the referees agree that it was inside limits.
+Given the decision of the 4 referees, help Chef determine whether the ball is considered inside limits or not.
+Input Format
+• The first line of input will contain a single integer 1, denoting the number of test cases.
+• Each test case consists of a single line of input containing 4 integers R1, R2, R3, R, denoting the decision of
+the respective referees.
+Here R can be either 0 or 1 where 0 would denote that the referee considered the ball to be inside limits
+whereas 1 denotes that they consider it to be outside limits.
+Output Format
+For each test case, output IN if the ball is considered to be inside limits by all referees and ouT otherwise.
+The checker is case-insensitive so answers like in, In, and IN would be considered the same.
+*/
