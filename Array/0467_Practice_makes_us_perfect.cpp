1b101f619680d4ac1757f90bc98e2e5d50d83b5d
Vikash Kumar <rajvikash18113@gmail.com>
2024-10-21 21:19:11 +0530

    Create 0467_Practice_makes_us_perfect.cpp

---
+++ Array/0467_Practice_makes_us_perfect.cpp
@@ -0,0 +1,23 @@
+// https://www.codechef.com/problems/PRACTICEPERF
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int arr[4];
+    int count = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int j = 0; j < 4; j++)
+    {
+        if (arr[j] >= 10)
+        {
+            count++;
+        }
+    }
+    cout << count << endl;
+}
